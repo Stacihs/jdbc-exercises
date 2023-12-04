76080ad2fec7ba464aebd7323df86b8d392768b9
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-12-04 14:22:58 -0600

    Complete App

--- src/main/java/QuotesTest.java
+++ src/main/java/QuotesTest.java
@@ -1,55 +1,19 @@
-import com.mysql.cj.jdbc.Driver;
-import config.Config;
+import dao.MySQLQuotesDAO;
+import models.Quote;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.JarOutputStream;
 
 public class QuotesTest {
 
-    public static List<String> getQuotes() {
-        Connection connection = null;
-        List<String> quotes = new ArrayList<>();
-        try {
-            //register driver
-            DriverManager.registerDriver(new Driver());
-
-            //establish connection
-            connection = DriverManager.getConnection(
-                    Config.getUrl(),
-                    Config.getUser(),
-                    Config.getPassword()
-            );
-
-            //Create statement object
-            Statement statement = connection.createStatement();
-
-            //Execute statement
-            ResultSet resultSet = statement.executeQuery("SELECT content FROM quotes");
-            while(resultSet.next()){
-                quotes.add(resultSet.getString("content"));
-            }
-
-
-        } catch (SQLException sqlx){
-            System.out.println(sqlx.getMessage());
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException sqlx){
-                    System.out.println(sqlx.getMessage());
-                }
-            }
-        }
-        return quotes;
-
-    }
-
     public static void main(String[] args) {
-        List<String> quotesFromDb = getQuotes();
-        for (String quote : quotesFromDb) {
-            System.out.println(quote);
+        MySQLQuotesDAO quotesDAO = new MySQLQuotesDAO();
+        quotesDAO.createConnection();
+        List<Quote> quotesFromDb = quotesDAO.getQuotes();
+        for (Quote quote : quotesFromDb) {
+            System.out.println(quote.getAuthor() + " said:");
+            System.out.println(quote.getContent());
         }
+        quotesDAO.closeConnection();
     }
 }
---
+++ src/main/java/dao/MySQLQuotesDAO.java
@@ -0,0 +1,75 @@
+package dao;
+
+import com.mysql.cj.jdbc.Driver;
+import config.Config;
+import models.Quote;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLQuotesDAO {
+    private Connection connection = null;
+
+    public void createConnection() {
+        System.out.println("Connecting to db...");
+        try {
+            //register driver
+            DriverManager.registerDriver(new Driver());
+
+            //establish connection
+            connection = DriverManager.getConnection(
+                    Config.getUrl(),
+                    Config.getUser(),
+                    Config.getPassword()
+            );
+        } catch (SQLException sqlx) {
+            System.out.println("Error connection to db..." + sqlx.getMessage());
+        }
+
+    }
+
+    public List<Quote> getQuotes() {
+        List<Quote> quotes = new ArrayList<>();
+        try {
+
+            //Create statement object
+            Statement statement = connection.createStatement();
+
+            //Execute statement
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM quotes");
+            while (resultSet.next()) {
+                quotes.add(new Quote(
+                        resultSet.getString("author"),
+                        resultSet.getString("content")
+                ));
+            }
+
+        } catch (SQLException sqlx) {
+            System.out.println(sqlx.getMessage());
+        }
+
+        return quotes;
+    }
+
+    public void closeConnection() {
+        System.out.println("Closing db connection...");
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException sqlx) {
+                System.out.println(sqlx.getMessage());
+            }
+        }
+    }
+
+    public long insertQuotes() {
+        //create connection
+
+        //execute statement
+
+        //close connection
+
+        return 0;
+    }
+}
